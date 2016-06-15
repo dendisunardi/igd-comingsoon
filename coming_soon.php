d820504d626532b4724ac6d071340e99f445b63e
dendisunardi <dendi.sunardi@gmail.com>
2016-06-16 05:19:01 +0700

    initialize project

---
+++ coming_soon.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>idg</title>
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
+</head>
+<body>
+<div id="image">
+ <div id="overlay">
+  <div id="heading_container">
+   <div class="main-logo">
+				<!-- <img src="assets/images/contoh.png"> -->
+				<div style="font-size: 24px">idg logo</div>
+			</div>
+   <p>Something new is coming, we are working on it. Please comeback soon. Be notified when it launches, please enter your email address.</p>
+   <div class="input-group email-input">
+			    <input type="text" class="form-control" placeholder="Enter your email" size=30>
+			    	<span class="input-group-btn" style="width:0;">
+			        	<button id="emailbtn" class="btn btn-custom-black" type="button">Notify me!</button>
+			    	</span>
+				</div>
+  </div>
+ </div>
+</div>
+</div>
+</body>
+</html>
